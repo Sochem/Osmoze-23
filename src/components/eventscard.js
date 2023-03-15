32e202a954f3fcfca3b0753619ff9f2d005cafd3
Sahil Saini <76941329+sahilsaini110@users.noreply.github.com>
2023-03-15 06:18:58 +0000

    card improved

--- src/components/eventscard.js
+++ src/components/eventscard.js
@@ -5,46 +5,46 @@ import data from './../../data/data';
 
 const eventscard = (props) => {
 
-    const type = props.type
+  const type = props.type
 
   return (
-    <div className="bg-gray-900">
-    <div className="grid grid-cols-3 gap-6 py-32">
-      {data.events.map((item) => {
-        if(type==="10"){
+    <div className="bg-gray-600">
+      <div className="grid grid-cols-3 gap-x-8 gap-y-12 py-16">
+        {data.events.map((item) => {
+          if (type === "10") {
             return (
-            <div>
-                    <Card
-                        key={item.id}
-                        name={item.name}
-                        image={item.image}
-                        designation={item.designation}
-                        review={item.review}
-                    />
-                </div>
-        );
-        }
-        else if(item.type === type){
-            return (
-            <div>
-                    <Card
-                        key={item.id}
-                        name={item.name}
-                        image={item.image}
-                        designation={item.designation}
-                        review={item.review}
-                    />
-                </div>
-        );
-        }
-        else{
-            return(
-                <></>
+              <div>
+                <Card
+                  key={item.id}
+                  name={item.name}
+                  image={item.image}
+                  designation={item.designation}
+                  review={item.review}
+                />
+              </div>
             );
-        }
-      })}
+          }
+          else if (item.type === type) {
+            return (
+              <div>
+                <Card
+                  key={item.id}
+                  name={item.name}
+                  image={item.image}
+                  designation={item.designation}
+                  review={item.review}
+                />
+              </div>
+            );
+          }
+          else {
+            return (
+              <></>
+            );
+          }
+        })}
+      </div>
     </div>
-  </div>
   )
 }
 
