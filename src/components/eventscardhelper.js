ac60cee658ce2f35c7f9695c3140c845ae9c5d95
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2023-03-17 00:22:33 +0530

    Fixed Image issue

--- src/components/eventscardhelper.js
+++ src/components/eventscardhelper.js
@@ -5,19 +5,20 @@ export default function Card(props) {
         <p className="heading">
           {props.name}
         </p>
-        {/* <p className="sm:mt-[0.625rem] font-custom font-normal text-xs sm:text-[1.125rem] text-[#FFFFFF]">
+        <p className="sm:mt-[0.625rem] font-custom font-normal text-xs sm:text-[1.125rem] text-[#FFFFFF]">
           {props.designation}
-        </p> */}
-        {/* <div>
+        </p>
+        <div>
             <Image
+            alt="Image"
             src={props.image}
             width={200}
             height={200}
             />
-        </div> */}
-        <p className="content">
-          {props.review}
-        </p>
+        </div>
+        <a className="content py-2" href={props.review}>
+          Link
+        </a>
       </div>
     );
   }
