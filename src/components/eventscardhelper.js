f412a151eb34bb85ff708131b07d65a9da2d7613
Abhinav Jha <linuxlenin04@gmail.com>
2023-03-16 23:43:17 +0530

    Events database integration done(almost)

--- src/components/eventscardhelper.js
+++ src/components/eventscardhelper.js
@@ -8,13 +8,13 @@ export default function Card(props) {
         {/* <p className="sm:mt-[0.625rem] font-custom font-normal text-xs sm:text-[1.125rem] text-[#FFFFFF]">
           {props.designation}
         </p> */}
-        <div>
+        {/* <div>
             <Image
             src={props.image}
             width={200}
             height={200}
             />
-        </div>
+        </div> */}
         <p className="content">
           {props.review}
         </p>
