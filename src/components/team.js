bf3c981a9cfd720032906edd3c61975c69fc2758
Sahil Saini <76941329+sahilsaini110@users.noreply.github.com>
2023-03-17 15:30:53 +0000

    updated margins

--- src/components/team.js
+++ src/components/team.js
@@ -6,8 +6,8 @@ export default function Team() {
         <div className="mt-[10.375rem] sm:mx-[10.625rem] pb-28 border-b-2 border-b-black">
 
             <div className="flex flex-col sm:flex-row justify-center">
-                <div className="hidden sm:block text-right mr-12 font-extrabold text-[64px] tracking-widest">Meet the Team <br></br>2022-23</div>
-                <div className="hidden sm:block px-16 text-left ml-12 font-extrabold text-[36px] tracking-widest">Know the amazing<br></br> individuals who make <br></br>our team a winning<br></br>combination.</div>
+                <div className="hidden sm:block text-right mr-12 font-extrabold text-[48px] tracking-widest">Meet the Team <br></br>2022-23</div>
+                <div className="hidden sm:block px-16 text-left ml-12 font-extrabold text-[30px] tracking-widest">Know the amazing<br></br> individuals who make <br></br>our team a winning<br></br>combination.</div>
                 <div className="sm:hidden text-center font-extrabold text-[24px] tracking-widest">Meet the Team <br></br>of 2022-23</div>
             </div>
 
