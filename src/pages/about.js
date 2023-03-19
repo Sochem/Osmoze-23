071e01957a3b06bee337941c381270ec1da4a4d7
Devansh Mishra <79010144+devansh12b2@users.noreply.github.com>
2023-03-19 05:46:43 +0000

    about us content changed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,67 +1,92 @@
-import Navbar from '@/components/navbar'
-import Footer from "../components/footer"
-import React from 'react'
-import Image from "next/image"
+import Navbar from "@/components/navbar";
+import Footer from "../components/footer";
+import React from "react";
+import Image from "next/image";
 
 const about = () => {
-    return (
-        <div>
-  {/* Hello world */}
-  <Navbar></Navbar>
-  <div>
-  <section className="section2" id="AboutUs">
-    <table className='mt-[17rem]'>
-      <tbody>
-        <tr>
-          <td>
-          <Image src={"/images/image 38.svg"} width={720} height={593} style ={{background: "#7EAF4B"}}/>
-          </td>
-          <td>
-            <span className="title1">About Us</span>
-            <span className="sub-title">
-            Who are we?
-            </span>
-            <p>
-            The society involves various budding chemical engineers from all the years. The society provides a platform for students to improve on their skills & personality. It also brings back the experience of our esteemed alumni for the benefit of the students. Various activities such as workshops, GDs, case studies, placement & internship sessions are organized by the society throughout the year.
-            </p>
-            <p>
-            The society also organizes its departmental fest: Osmoze once in a year which extract out the prestigious innovations of students.
-            </p>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
-  <section className="section3" id="OurMission">
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            <span className="title2">Our Mission</span>
-            <p>
-            The society involves various budding chemical engineers from all the years. The society provides a platform for students to improve on their skills & personality. It also brings back the experience of our esteemed alumni for the benefit of the students. Various activities such as workshops, GDs, case studies, placement & internship sessions are organized by the society throughout the year.
+  return (
+    <div>
+      {/* Hello world */}
+      <Navbar></Navbar>
+      <div>
+        <section className="section2" id="AboutUs">
+          <table className="mt-[17rem]">
+            <tbody>
+              <tr>
+                <td>
+                  <Image
+                    src={"/images/image 38.svg"}
+                    width={720}
+                    height={593}
+                    style={{ background: "#7EAF4B" }}
+                  />
+                </td>
+                <td>
+                  <span className="title1">About Us</span>
+                  <span className="sub-title">Who are we?</span>
+                  <p>
+                    The Society of Chemical Engineers a.k.a SoChem is a society
+                    run by the students of the department for the students of
+                    the department. SoChem ensures that each and every student
+                    within the department gets the most out of their time with
+                    us at IIT(BHU) Varanasi.
+                  </p>
+                
+                  <p>
+                    The society as a whole has strong links with various
+                    industries in the engineering and financial sectors. The
+                    society organizes presentations from company representatives
+                    and application days, held within the department itself,
+                    directly aimed at chemical engineering students.
+                  </p>
+                  <p>
+                    The society also organizes its departmental fest: Osmoze
+                    once in a year which extract out the prestigious innovations
+                    of students.
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <section className="section3" id="OurMission">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <span className="title2">Our Mission</span>
+                  <p>
+                    The society involves various budding chemical engineers from
+                    all the years. The society provides a platform for students
+                    to improve on their skills & personality. It also brings
+                    back the experience of our esteemed alumni for the benefit
+                    of the students. Various activities such as workshops, GDs,
+                    case studies, placement & internship sessions are organized
+                    by the society throughout the year.
+                  </p>
+                  <p>
+                    The society also organizes its departmental fest: Osmoze
+                    once in a year which extract out the prestigious innovations
+                    of students.
+                  </p>
+                </td>
+                <td>
+                  <Image
+                    src={"/images/Group 4678.svg"}
+                    width={720}
+                    height={593}
+                    style={{ background: "#FFB109" }}
+                  />
+                </td>
+              </tr>
+            </tbody>
+            {/* <Footer> </Footer> */}
+          </table>
+        </section>
+      </div>
+      <Footer> </Footer>
+    </div>
+  );
+};
 
-            </p>
-            <p>
-            The society also organizes its departmental fest: Osmoze once in a year which extract out the prestigious innovations of students.
-
-            </p>
-          </td>
-          <td>
-           <Image src={"/images/Group 4678.svg"} width={720} height={593} style ={{background: "#FFB109"}}/>
-          </td>
-        </tr>
-      </tbody>
-      {/* <Footer> </Footer> */}
-    </table>
-  
-  
-  </section>
-  </div>
-  <Footer> </Footer>
-</div>
-
-    )
-}
-
-export default about
+export default about;
