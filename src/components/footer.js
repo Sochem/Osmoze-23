99557dfeb5256b707333c07afccf8cd27e1198ed
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2023-03-21 00:57:51 +0530

    Homepage mobile view

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,12 +16,12 @@ export default function Navbar() {
 						<div className="img"><Link href="https://www.instagram.com"><Image src="/images/social2.svg" width={40} height={40} /></Link></div>
 						<div className="img"><Link href="https://www.linkedin.com"><Image src="/images/social3.svg" width={40} height={40} /></Link></div>
 					</div>
-					<div className="footer-text-1">
-						<div className="contact">
+					<div className="flex flex-col sm:flex-row">
+						<div className="p-6 text-[24px]">
 							<div className="cc">Aditya Kumar Singh</div>
 						    <div>+91 9507510924</div>
 						</div>
-						<div className="contact">
+						<div className="p-6 text-[24px]">
 							<div className="cc">Prince</div>
 						    <div>+91 9065765411</div>
 						</div>
