e741a4df93f19d462f1b4a9eb536095d85188e26
Pranjal <pranjal.rajput.che21@itbhu.ac.in>
2023-03-21 01:27:46 +0530

    Created Announcement Page

---
+++ src/components/announcementHelper.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function announcementHelper() {
+	return (
+		<div>
+			<div className="boxComponent">
+				<div className="leftBox">
+					OSMOCLICK<div className="date"> April 8 2022 </div>
+				</div>
+				<div className="rightBox">
+					Result - Symposium consisted of two rounds, first of which
+					was group discussion followed by turncoat.
+					<br />
+					<br />
+					Winners from 2nd year
+					<br />
+					Shivam Gupta
+					<br />
+					Aditya Kumar Singh
+					<br />
+					Gunjan Pandey
+					<br />
+					<br />
+					Winners from 1st year
+					<br />
+					Naman Lakhotia
+					<br />
+					Saransh Mishra
+					<br />
+					Abhishek Kumar
+					<br />
+					Kudos to the winners!
+				</div>
+			</div>
+		</div>
+	);
+}
